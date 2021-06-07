211b1682ab54440226b9dffd06eeedd7a6a9d9a4
BlessonTh <blesson.thomas2020@vitbhopal.ac.in>
2021-06-07 15:21:48 +0530

    second largest

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    import sys
+    Max=arr[0]
+    r=sys.maxsize
+    for i in range(0,n):
+        if arr[i]>Max:
+            r=Max
+            Max=arr[i]
+        elif arr[i]==Max:
+            continue
+        elif arr[i]<Max:
+            if arr[i]>r:
+                r=arr[i]
+            
+    print(r)
