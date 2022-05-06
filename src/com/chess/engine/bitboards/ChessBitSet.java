a0c9d31852a9f5ade99081a1a71aa0695614e3a1
BlessonTh <blesson.thomas2020@vitbhopal.ac.in>
2022-05-07 03:53:09 +0530

    Modifv1

--- src/com/chess/engine/bitboards/ChessBitSet.java
+++ src/com/chess/engine/bitboards/ChessBitSet.java
@@ -22,7 +22,48 @@ public final class ChessBitSet extends BitSet {
 		words[words.length - 1] >>>= n; // shift [words.length-1] separately, since no carry
 		return BitSet.valueOf(words);
 	}
+	
+    public BitSet shiftRight(BitSet bits, int n) {
+        long[] words = bits.toLongArray();
+        // Expand array if there will be carry bits
+        if (words[words.length - 1] >>> n > 0) {
+            long[] tmp = new long[words.length + 1];
+            System.arraycopy(words, 0, tmp, 0, words.length);
+            words = tmp;
+        }
+        // Do the shift
+        for (int i = words.length - 1; i > 0; i--) {
+            words[i] <<= n; // Shift current word
+            words[i] |= words[i - 1] >>> (64 - n); // Do the carry
+        }
+        words[0] <<= n; // shift [0] separately, since no carry
+        return BitSet.valueOf(words);
+    }
 
+	public ChessBitSet shift(final int shiftValue) {
+		final int len = length();
+		if (shiftValue > 0) {
+			if (len + shiftValue < 64) {
+				for (int bitIndex = len; bitIndex >= 0; bitIndex--) {
+					set(bitIndex + shiftValue, get(bitIndex));
+				}
+				clear(0, shiftValue);
+			} else {
+				clear(shiftValue, len + shiftValue);
+			}
+		} else if (shiftValue < 0) {
+			if (len < -shiftValue) {
+				clear();
+			} else {
+				for (int bitIndex = -shiftValue; bitIndex < length(); bitIndex++) {
+					set(bitIndex + shiftValue, get(bitIndex));
+				}
+				clear(len + shiftValue, len);
+			}
+		}
+
+		return this;
+	}
 
 	@Override
 	public String toString() {
